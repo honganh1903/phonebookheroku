7c3b248ef969904860b5ea80ec5fd959cbb9bff6
LAPTOP-3RHO7SBV\Asus <lehonganh1903@gmail.com>
2022-07-25 10:02:14 +0700

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const app = express()
-app.use(express.static('/build'))
+app.use(express.static('build'))
 let persons = [
     { 
       id: 1,
