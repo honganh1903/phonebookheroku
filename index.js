776976f1a728efd68e2ed6f30ddaa404d5063eca
LAPTOP-3RHO7SBV\Asus <lehonganh1903@gmail.com>
2022-07-25 09:54:17 +0700

    heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const app = express()
-
+app.use(express.static('/build'))
 let persons = [
     { 
       id: 1,
@@ -86,7 +86,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
